dd3452507a81d8e8668f2299a63e85b14cb2f956
jess-icaa <jessicashalomin@gmail.com>
2026-03-30 19:18:15 +0530

    fix dotenv for production

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
+console.log("ENV CHECK:", process.env.MONGODB_URI); 
 const app = require("./src/app");
 const connectDB = require("./src/config/db.config");
+
 
 const PORT = process.env.PORT || 5001;
 
