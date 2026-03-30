5e1b08a77c9c9c41501417b90c3fab3d9c31a150
jess-icaa <jessicashalomin@gmail.com>
2026-03-30 19:21:31 +0530

    fix dotenv for production

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
