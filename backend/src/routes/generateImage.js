4193c2f76fc7948a6c5c1a0ae9df4afac9a95c37
jess-icaa <jessicashalomin@gmail.com>
2026-04-06 16:03:09 +0530

    updated code

--- backend/src/routes/generateImage.js
+++ backend/src/routes/generateImage.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const fetch = require("node-fetch");
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const router = express.Router();
 
