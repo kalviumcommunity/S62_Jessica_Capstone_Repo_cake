2a954bca4529b205b5cb6cbbaf1f9a425569eff1
jess-icaa <jessicashalomin@gmail.com>
2026-04-06 16:18:15 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,15 +12,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://s62-jessica-capstone-repo-cake.vercel.app"
-    ],
-    credentials: true,
-  })
-);
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://s62-jessica-capstone-repo-cake-6e4ga62yk.vercel.app"
+  ],
+  credentials: true
+}));
 
 app.use("/api/cakes", cakeRoutes);
 app.use("/api/auth", authRoutes);
